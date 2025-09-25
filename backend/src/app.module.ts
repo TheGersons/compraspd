06bc5b5d8e1dbb93e26ca743d549fc1c409baa4b
TheGersons <gersonmurillo2711@gmail.com>
2025-09-25 13:51:06 -0600

    Mejoramiento en la estructura y organizacion de las carpetas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { SuppliersCrudModule } from './tue/suppliers--crud/suppliers--crud.module';
-import { ProductsCrudModule } from './tue/products--crud/products--crud.module';
-import { QuotesCrudModule } from './tue/quotes--crud/quotes--crud.module';
-import { PurchaseOrdersCrudModule } from './tue/purchase-orders--crud/purchase-orders--crud.module';
+import { SuppliersCrudModule } from './suppliers/suppliers--crud.module';
+import { ProductsCrudModule } from './products/products--crud.module';
+import { QuotesCrudModule } from './quotes/quotes--crud.module';
+import { PurchaseOrdersCrudModule } from './purchases/purchase-orders--crud.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, UsersModule, SuppliersCrudModule, ProductsCrudModule, QuotesCrudModule, PurchaseOrdersCrudModule],
