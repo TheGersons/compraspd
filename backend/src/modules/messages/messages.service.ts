f39b1100e9a88329b52d55ff9c09325dd82a2763
TheGersons <gersonmurillo2711@gmail.com>
2026-04-07 13:54:14 -0600

    fix

--- backend/src/modules/messages/messages.service.ts
+++ backend/src/modules/messages/messages.service.ts
@@ -594,7 +594,7 @@ export class MessagesService {
     senderId: string,
     messageContent: string,
   ): Promise<void> {
-    const frontendUrl = process.env.FRONTEND_URL_SANDBOX || process.env.FRONTEND_URL || 'http://89.167.20.163:8080';
+    const frontendUrl = 'http://89.167.20.163:8080';
 
     // Obtener todos los participantes actuales con sus datos de usuario
     const participantes = await this.prisma.participantesChat.findMany({
